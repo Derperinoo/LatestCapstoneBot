77b44974532f4180aad08a828308272c13d6334a
unknown <tabaconm39@gmail.com>
2018-08-01 18:50:03 +0800

    Pushing

--- index.js
+++ index.js
@@ -36,11 +36,7 @@ app.post('/webhook/', function(req, res){
 		if(event.message && event.message.text){
 			let text = event.message.text
 
-			if(text.includes("happy")
-
-				sendText(sender, "wew" + text.substring(0, 100))
-			)
-			
+			sendText(sender,  text.substring(0, 100))
 		}
 	}
 	res.sendStatus(200)
