b7c131e87887ddaf513374ad96adf8cded78fe97
unknown <tabaconm39@gmail.com>
2018-08-01 18:37:38 +0800

    push

--- index.js
+++ index.js
@@ -35,8 +35,11 @@ app.post('/webhook/', function(req, res){
 		let sender = event.sender.id
 		if(event.message && event.message.text){
 			let text = event.message.text
-			sendText(sender,  text.substring(0, 100))
-		}
+
+			if(text.includes("happy"){
+			sendText(sender, "hallo:" + text.substring(0, 100))
+			})
+			
 	}
 	res.sendStatus(200)
 })
