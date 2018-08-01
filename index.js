88984cd5fb615a5a7e6e6442e44a7a429b5f0e99
unknown <tabaconm39@gmail.com>
2018-08-01 19:04:31 +0800

    waw

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook/', function(req, res){
 		let sender = event.sender.id
 		if(event.message && event.message.text){
 			let text = event.message.text
-			sendText(sender, text.substring(0, 100))
+			sendText(sender, "text echo:" + text.substring(0,100))
 		}
 	}
 	res.sendStatus(200)
