89ac26651de195c0de6b6cb4a25596ef4fd40d49
unknown <tabaconm39@gmail.com>
2018-08-01 19:27:20 +0800

    wawiks

--- index.js
+++ index.js
@@ -14,34 +14,34 @@ app.use(bodyParser.json())
 
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.send("Hi I am a chatbot")
 })
 
 let token = "EAAimrKFwRkIBALmmo97tyXw7a5LZA25Iy3c1W4kWgODAXvMdPRdO0rD79aVyBChfIYSDTXnOGQ5eIl0kNjeIBobiyXwvPzZByD6LrNZCx1btxQBOs1Tk9CZCxSfbwhWMlmsKAmA83VrKv48ZB4B9ZCMdafv0VK4ZAzcrkmRN0HuaQZDZD"
 
 
-app.get('/webhook/', function(req, res){
-	if(req.query['hub.verify_token'] === "Hi"){
+app.get('/webhook/', function(req, res) {
+	if(req.query['hub.verify_token'] === "Hi") {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token")
 })
 
-app.post('/webhook/', function(req, res){
+app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
-	for(let i = 0; i < messaging_events.length; i++){
+	for(let i = 0; i < messaging_events.length; i++) {
 		let event = messaging_events[i]
 		let sender = event.sender.id
-		if(event.message && event.message.text){
+		if(event.message && event.message.text) {
 			let text = event.message.text
-			
+			sendText(sender, "text echo: " + text.substring(0,100))
 		}
 	}
 	res.sendStatus(200)
 })
 
-function sendText(sender, text){
+function sendText(sender, text) {
 	let messageData = {text: text}
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
@@ -51,15 +51,15 @@ function sendText(sender, text){
 			recipient: {id: sender},
 			message : messageData,
 		}
-	}, function(error, response, body){
-		if(error){
+	}, function(error, response, body) {
+		if(error) {
 			console.log("sending error")
-		}else if (response.body.error){
+		}else if (response.body.error) {
 			console.log("response body error")
 		}
 	})
 }
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
 	console.log("running: port")
 })
