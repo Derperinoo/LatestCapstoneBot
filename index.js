5d851a4afae25e6c775e41f165985d4490e55218
tabacon123 <tabaconm39@gmail.com>
2018-09-15 03:12:10 +0800

    adring

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/equirino',function(_req, _res){
 	  	const int5 = body.RWS[0].RW[0].FIS[0].FI[4].TMC.DE;
 	  	const jf5 = body.RWS[0].RW[0].FIS[0].FI[4].CF[0].JF;
 
-	  	const y = 5
+	  	var y = 5
 	  
 	  	var sum = jf1 + jf2 + jf3 + jf4 + jf5 / y;
 
@@ -123,14 +123,6 @@ app.get('/jplaurel',function(_req, _res){
 
 	request('https://traffic.api.here.com/traffic/6.1/flow.json?bbox=7.2598%2C125.0860%3B6.7670%2C125.6674&app_id=fQbW8CGYiU3l5mLqWgBE&app_code=SYZXwjFBHSYi_1t1GNuHow', { json: true }, (err, res, body) => {
 	  if (err) { return console.log(err); }
-	  	// console.log(body.url);
-	  	// console.log(body.explanation);
-	  	// console.log(body.RWS[0].RW);
-	  	// console.log("###################");
-	  	// console.log(body.RWS[0].RW[2].DE);
-	  	// console.log(body.RWS[0].RW[0].FIS[0].FI[0].TMC.DE);
-	  	
-
 	  	
 	  	const sep = ("----------------------");
 
@@ -215,6 +207,125 @@ app.get('/jplaurel',function(_req, _res){
 
 
 });
+app.get('/mcarthur',function(_req, _res){
+
+	request('https://traffic.api.here.com/traffic/6.1/flow.json?bbox=7.2598%2C125.0860%3B6.7670%2C125.6674&app_id=fQbW8CGYiU3l5mLqWgBE&app_code=SYZXwjFBHSYi_1t1GNuHow', { json: true }, (err, res, body) => {
+	  if (err) { return console.log(err); }
+	  	
+	  	const sep = ("----------------------");
+
+	  	const sep1 = ("----------------------");
+
+	  	const sep2 = ("----------------------");
+
+	  	const sep3 = ("----------------------");
+
+	  	const sep4 = ("----------------------");
+
+	  	const streetmm = body.RWS[0].RW[5].DE;
+	  	const intm = body.RWS[0].RW[5].FIS[0].FI[0].TMC.DE;
+	  	const jfm = body.RWS[0].RW[5].FIS[0].FI[0].CF[0].JF;
+	  	
+	  	const intm1 = body.RWS[0].RW[5].FIS[0].FI[1].TMC.DE;
+	  	const jf02 = body.RWS[0].RW[5].FIS[0].FI[1].CF[0].JF;
+
+	  	const intm2 = body.RWS[0].RW[5].FIS[0].FI[2].TMC.DE;
+	  	const jfm2 = body.RWS[0].RW[5].FIS[0].FI[2].CF[0].JF;
+
+	  	const intm3 = body.RWS[0].RW[5].FIS[0].FI[3].TMC.DE;
+	  	const jfm3 = body.RWS[0].RW[5].FIS[0].FI[3].CF[0].JF;
+
+	  	const intm4 = body.RWS[0].RW[5].FIS[0].FI[4].TMC.DE;
+	  	const jfm4 = body.RWS[0].RW[5].FIS[0].FI[4].CF[0].JF;
+
+	  	const intm5 = body.RWS[0].RW[5].FIS[0].FI[5].TMC.DE;
+	  	const jfm5 = body.RWS[0].RW[5].FIS[0].FI[5].CF[0].JF;
+
+	  	const intm6 = body.RWS[0].RW[5].FIS[0].FI[6].TMC.DE;
+	  	const jfm6 = body.RWS[0].RW[5].FIS[0].FI[6].CF[0].JF;
+
+	  	const intm7 = body.RWS[0].RW[5].FIS[0].FI[7].TMC.DE;
+	  	const jfm7 = body.RWS[0].RW[5].FIS[0].FI[7].CF[0].JF;
+
+	  	const intm8 = body.RWS[0].RW[5].FIS[0].FI[8].TMC.DE;
+	  	const jfm8 = body.RWS[0].RW[5].FIS[0].FI[8].CF[0].JF;
+
+	  	const intm9 = body.RWS[0].RW[5].FIS[0].FI[9].TMC.DE;
+	  	const jfm9 = body.RWS[0].RW[5].FIS[0].FI[9].CF[0].JF;
+
+	  	const intm10 = body.RWS[0].RW[5].FIS[0].FI[10].TMC.DE;
+	  	const jfm10  = body.RWS[0].RW[5].FIS[0].FI[10].CF[0].JF;
+	  		
+	  	const intm11 = body.RWS[0].RW[5].FIS[0].FI[11].TMC.DE;
+	  	const jfm11 = body.RWS[0].RW[5].FIS[0].FI[11].CF[0].JF;
+	  	
+	  	const intm12 = body.RWS[0].RW[5].FIS[0].FI[12].TMC.DE;
+	  	const jfm12 = body.RWS[0].RW[5].FIS[0].FI[12].CF[0].JF;
+	  	
+	  	const intm13 = body.RWS[0].RW[5].FIS[0].FI[13].TMC.DE;
+	  	const jfm13 = body.RWS[0].RW[5].FIS[0].FI[13].CF[0].JF;
+
+	  	const x = 14
+	  
+	  	var sum1 = jfm + jf02 + jfm2 + jfm3 + jfm4 + jfm5 + jfm6 + jfm8
+	  	+ jfm9 + jfm10 + jfm11 + jfm12 + jfm13  / x;
+
+	  	
+	  	let analysis1 = "";
+	  	if(sum1 <= 4){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 3){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 2){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 3){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 2){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 1){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 0){
+	  		analysis1 = "Free flow of traffic";
+	  	}else if(sum1 <= 8){
+	  		analysis1 = "Sluggish flow of traffic";
+	  	}else if(sum1 <= 7){
+	  		analysis1 = "Sluggish flow of traffic"
+	  	}else if(sum1 <= 6){
+	  		analysis1 = "Sluggish flow of traffic"
+	  	}else if(sum1 <= 5){
+	  		analysis1 = "Sluggish flow of traffic"
+	  	}else if(sum1 <= 4){
+	  		analysis1 = "Sluggish flow of traffic"
+	  	}else if(sum1 <= 8){
+	  		analysis1 = "Slow flow of traffic"
+	  	}else if(sum1 <= 9){
+	  		analysis1 = "Slow flow of traffic"
+	  	}else if(sum1 <= 10){
+	  		analysis1 = "Slow flow of traffic"
+	  	}else{
+	  		analysis1 = "dili nako ma computer"
+	  	}
+
+
+
+	  	_res.setHeader('Content-Type', 'application/json');
+    	_res.send(JSON.stringify({ streetmm: streetmm, sep1:sep1, intm1: intm1, jfm: jfm, sep: sep, intm2: intm2, jfm2: jfm2, sep2: sep2, intm3: intm3, 
+    		jfm3: jfm3, sep3: sep3, 
+    		intm4: intm4, jfm4: jfm4, sep4: sep4, intm5: intm5, jfm5: jfm5, sep: sep, intm6: intm6, jfm6: jfm6, sep4: sep4, intm7: intm7, 
+    		jfm7: jfm7, sep4: sep4, intm8: intm8, jfm8: jfm8, sep2: sep2, intm9: intm9, jfm9: jfm9, intm10: intm10, jfm10: jfm10, 
+    		intm11: intm11, jfm11: jfm11, intm12: intm12, jfm13: jfm13, jfmsepanalysis1: analysis1 }));
+	
+
+
+
+	  
+	});
+
+
+});
+
+
+
 
 app.get('/geo',function(req, res){
 	// res.send('Hi geo');
@@ -265,6 +376,23 @@ app.get('/geo',function(req, res){
 app.get('/geo',function(req, res){
 	
 
+	axios.get('https://polar-castle-83452.herokuapp.com/mcarthur')
+	  .then(function (response) {
+	    console.log(response.data);
+	    //chatbotResponse = response.jf1;
+	    //sendText(sender, chatbotResponse)
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	    //chatbotResponse = "not ok";
+	    //sendText(sender, chatbotResponse)
+	  });
+
+})
+
+app.get('/geo',function(req, res){
+	
+
 	axios.get('https://polar-castle-83452.herokuapp.com/jplaurel')
 	  .then(function (response) {
 	    console.log(response.data);
@@ -278,6 +406,9 @@ app.get('/geo',function(req, res){
 	  });
 
 })
+
+
+ 
 
 let token = "EAAimrKFwRkIBADU9ZAgmFJPZBZB2hipmHHPdMFp0X8soLUq1ElyX0v20noVjGBvuyjppZBEHKaR1rhZAjCM9eI7nWG5JiRCaJISuLo8Oxfp9KDxx489dj5ukAReIgSc0c1IOcHmzTFEovaCvRhGZCcZCMDnJL2D251mkZCnJjs8uggZDZD"
 
