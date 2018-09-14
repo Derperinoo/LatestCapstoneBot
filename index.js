3679091bf6f936f087bb0c5a269ac0745ab5c937
tabacon123 <tabaconm39@gmail.com>
2018-09-14 18:33:12 +0800

    itom

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ app.get('/equirino',function(_req, _res){
     	_res.send(JSON.stringify({ street: street, int1: int1, jf1: jf1, analysis: analysis }));
 	});
 
+
+	    const street2 = body.RWS[0].RW[0].DE;
+	  	const int2 = body.RWS[0].RW[0].FIS[0].FI[0].TMC.DE;
+	  	const jf2 = body.RWS[0].RW[0].FIS[0].FI[0].CF[1].JF;
+
+	    _res.setHeader('Content-Type', 'application/json');
+    	_res.send(JSON.stringify({ street2: street2, int2: int2, jf2: jf2 }));
     
 });
 
