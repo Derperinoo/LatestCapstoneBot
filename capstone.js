e2046c9d31298e7a4ef1cd8c34919da5473c2115
tabacon123 <tabaconm39@gmail.com>
2018-09-25 08:25:35 +0800

    key

--- capstone.js
+++ capstone.js
@@ -5031,6 +5031,7 @@ app.post('/webhook/', function(req, res) {
 				  .catch(function (error) {
 				    //console.log(error);
 				    chatbotResponse = "not ok";
+				    chatbotResponse1 = "not ok";
 				    sendText(sender, chatbotResponse)
 				    sendText(sender, chatbotResponse1)
 				  });
