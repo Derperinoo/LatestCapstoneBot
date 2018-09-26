a3775d0efcc0daecf4a345277379591b1c1fb4f8
tabacon123 <tabaconm39@gmail.com>
2018-09-27 00:55:13 +0800

    llllasdasd

--- capstone.js
+++ capstone.js
@@ -10677,30 +10677,25 @@ app.post('/webhook/', function(req, res) {
 				let chatbotResponse6 = "";
 				
 				
+				
 				//source : https://www.npmjs.com/package/axios
-				   axios.get('https://cryptic-eyrie-21978.herokuapp.com/diversionroad-')
+				   axios.get(' https://cryptic-eyrie-21978.herokuapp.com/diversionroad-')
 				  .then(function (response) {
 				
 
 				    chatbotResponse1 = response.data.analysis52;
 				    sendText(sender, chatbotResponse1)
-
 				    chatbotResponse2 = response.data.analysis53;
 				    sendText(sender, chatbotResponse2)
-
 				    chatbotResponse3 = response.data.analysis54;
 				    sendText(sender, chatbotResponse3)
-
 				    chatbotResponse4 = response.data.analysis55;
 				    sendText(sender, chatbotResponse4)
-
 				    chatbotResponse5 = response.data.analysis56;
 				    sendText(sender, chatbotResponse5)
-
-				    hatbotResponse6 = response.data.analysis57;
+				    chatbotResponse6 = response.data.analysis57;
 				    sendText(sender, chatbotResponse6)
-
-				   
+	  
 				  })
 				  .catch(function (error) {
 				    //console.log(error);
@@ -10710,13 +10705,15 @@ app.post('/webhook/', function(req, res) {
 				     chatbotResponse4 = "not ok";
 				     chatbotResponse5 = "not ok";
 				     chatbotResponse6 = "not ok";
-
+				   
 				    sendText(sender, chatbotResponse1)
 				    sendText(sender, chatbotResponse2)
 				    sendText(sender, chatbotResponse3)
 				    sendText(sender, chatbotResponse4)
 				    sendText(sender, chatbotResponse5)
 				    sendText(sender, chatbotResponse6)
+				    
+				    
 				    
 				  });
 
